41346d9b7abfab25d3e2f36120748095038d4336
Chaitanya Ravuri <20ChaituR@users.noreply.github.com>
2020-07-24 12:49:59 -0700

    Update learner response

--- app/server.py
+++ app/server.py
@@ -60,8 +60,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    prediction = str(learn.predict(img)[0]).replace('-', ' ').title()
+    return JSONResponse({'result': prediction})
 
 
 if __name__ == '__main__':
