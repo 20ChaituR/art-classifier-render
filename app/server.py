dc8abb8fd775a3f0727643a9b3b52c78bd09f6b4
Chaitanya Ravuri <20ChaituR@users.noreply.github.com>
2020-07-24 12:30:22 -0700

    Updated model to hw1-art

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1_9fasfKSv-V8zyi1EIanNn9Bv1ZuP680'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['abstract-expressionism', 'baroque-art', 'cubism', 'expressionism', 'impressionism', 'minimalism', 'modern-art', 'neoclassicism', 'post-impressionism', 'renaissance-art', 'rococo', 'surrealism']
 path = Path(__file__).parent
 
 app = Starlette()
